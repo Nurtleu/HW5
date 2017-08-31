2401f6f041d80244fae00a78f9c7292e81b405b0
Nurtleu <nur71701@gmail.com>
2017-08-31 19:18:20 +0600

    __hw_5__

---
+++ HomeWork5/Task1/task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task1
+{
+    class Program
+    {          
+        static void Main(string[] args)
+        {
+            WriteLine("Enter web property: ");
+            string web = ReadLine();
+            try
+            {
+                throw new Exception($"This resource does not exist!");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex);
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ HomeWork5/Task2/task2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[] { 0, 12, 25, 54, 35, 8, 10, 5 };
+            try
+            {
+                int index;
+                Write("Enter the index:");
+                Int32.TryParse(ReadLine(),out index);
+                if(index>mas.Length)
+                {
+                    throw new IndexOutOfRangeException("The index is beyond it!");
+                }
+
+                else
+                {
+                    Write(mas[index]);
+                }
+            }
+            catch(IndexOutOfRangeException Ex)
+            {
+                Write(Ex);
+            }
+            ReadLine();
+        }
+    }
+}
